7fc5c7027f50bd03e1c84f45b110b1aa83126e57
anthonysotoleandres <1012020192@unajma.edu.pe>
2023-05-08 17:47:46 -0500

    tercercommit

--- src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
+++ src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cliente-add-edit',
@@ -7,15 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteAddEditPage implements OnInit {
 
-  constructor() { }
+  clienteForm!: FormGroup;
 
-  nombre: string = "Anthony";
+  constructor(private formBuilder: FormBuilder) { }
+
+  nombre: string = "Anthony"; // esta leendo el formulario
+
+  cliente = {
+    nombre: "Anthony",  
+    email: "thonyvtl@gmail.com", // esta leendo el formulario
+  };
+
+
   ngOnInit() {
+    this.clienteForm = this.formBuilder.group({
+
+      //PARA EL NOMBRE
+      nombre: ['',
+
+      Validators.compose([
+
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(50),
+
+      ])],
+      
+      //PARA EMAIL
+      email: ['',
+      Validators.compose([
+        Validators.required,
+        Validators.email,
+
+      ])
+    ]
+
+
+
+    });
   }
 
   submit() {
     console.log(this.nombre);
-    this.nombre = "Hermenegildo";
+    this.nombre = "Hermenegildo"; // eveteno q se vera cuando haga clicl en button
     }
 
 }
